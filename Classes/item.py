7fa246c00be89cb4571cc6a4a300985fa85e659c
Sina Sen <sinasen2000@hotmail.com>
2019-05-07 17:35:26 +0200

    made a better gui, added items and healing system.

---
+++ Classes/item.py
@@ -0,0 +1,6 @@
+class Item:
+    def __init__(self, name, type, description, property):
+        self.name = name
+        self.type = type
+        self.description = description
+        self.property = property
